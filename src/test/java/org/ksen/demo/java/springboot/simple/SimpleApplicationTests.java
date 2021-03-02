2c2dc70cbaaafe8b461c515bd21693e838f1e8e3
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-03-02 12:51:10 +0200

    Initial commit

---
+++ src/test/java/org/ksen/demo/java/springboot/simple/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ksen.demo.java.springboot.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
